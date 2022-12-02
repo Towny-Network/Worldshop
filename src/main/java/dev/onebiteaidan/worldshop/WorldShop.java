040cc2d3214784a0ec5d7176ab962ccc52173000
adb1067 <35378466+OneBiteAidan@users.noreply.github.com>
2022-12-01 20:05:10 -0500

    Initial commit

---
+++ src/main/java/dev/onebiteaidan/worldshop/WorldShop.java
@@ -0,0 +1,17 @@
+package dev.onebiteaidan.worldshop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WorldShop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
