8709ecbc5d19da6551e081ddd5dbe61425244a87
OneBiteAidan <5730dogman@gmail.com>
2024-11-19 10:55:20 -0500

    fix: Reworked the Database implementation to be easily testable and extendable.

--- src/main/java/dev/onebiteaidan/worldshop/Model/DataManagement/Database/Database.java
+++ src/main/java/dev/onebiteaidan/worldshop/Model/DataManagement/Database/Database.java
@@ -1,50 +1,25 @@
 package dev.onebiteaidan.worldshop.Model.DataManagement.Database;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * SQL-type database driver interface.
+ * All databases run using the JDBC database driver.
+ */
 public interface Database {
 
-   enum Tables {
-      TRADES,
-      PICKUPS,
-      PLAYERS
-   }
-
-   enum TradeColumns {
-      ALL,              // Represents the '*' character
-      TRADE_ID,
-      SELLER_UUID,
-      BUYER_UUID,
-      ITEM_OFFERED,
-      ITEM_REQUESTED,
-      TRADE_STATUS,
-      LISTING_TIMESTAMP,
-      COMPLETION_TIMESTAMP
-   }
-
-   enum PickupColumns {
-      ALL,
-      PLAYER_UUID,
-      TRADE_ID,
-      COLLECTED,
-      COLLECTION_TIMESTAMP
-   }
-
-   enum PlayerColumns {
-      ALL,
-      PLAYER_UUID,
-      PURCHASES,
-      SALES
-   }
-
    void connect() throws SQLException;
+   void disconnect() throws SQLException;
    boolean isConnected();
-   void disconnect();
+
    Connection getConnection();
-   // Queries need to have the connection passed in because a ResultSet cannot be accessed after the database connection is closed.
-   void createTradesTable();
-   void createPickupsTable();
-   void createPlayersTable();
+
+
+   ResultSet executeQuery(PreparedStatement ps) throws SQLException;
+   int executeUpdate(PreparedStatement ps) throws SQLException;
+   boolean execute(PreparedStatement ps) throws SQLException;
 
 }
